51ba67ffed6dca34e7e4387425748fbbb879204e
Kaleb <kaleb@gullele.com>
2018-08-16 18:11:35 -0400

    Adding global CORS config

---
+++ src/main/java/com/solutionladder/ethearts/WebConfig.java
@@ -0,0 +1,22 @@
+package com.solutionladder.ethearts;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Adding CORS request from any origin
+ * 
+ * @author Kaleb Woldearegay <kaleb@solutionladder.com>
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+ 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
