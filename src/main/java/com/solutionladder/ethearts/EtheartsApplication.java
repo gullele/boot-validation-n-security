718cf0ba9855256ee9faa6729e4296d063afc036
Kaleb <kaleb@gullele.com>
2018-08-17 00:05:41 -0400

    Adding Options method

--- src/main/java/com/solutionladder/ethearts/EtheartsApplication.java
+++ src/main/java/com/solutionladder/ethearts/EtheartsApplication.java
@@ -1,14 +1,12 @@
 package com.solutionladder.ethearts;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class EtheartsApplication {
@@ -17,19 +15,17 @@ public class EtheartsApplication {
 		SpringApplication.run(EtheartsApplication.class, args);
 	}
 	
-	  @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        CorsConfiguration corsConfiguration = new CorsConfiguration();
-	        corsConfiguration.addAllowedOrigin("http://localhost:9090");
-	        corsConfiguration.setAllowedMethods(Arrays.asList(
-	                HttpMethod.GET.name(),
-	                HttpMethod.HEAD.name(),
-	                HttpMethod.POST.name(),
-	                HttpMethod.PUT.name(),
-	                HttpMethod.DELETE.name()));
-	        corsConfiguration.setMaxAge(1800L);
-	        source.registerCorsConfiguration("/**", corsConfiguration); // you restrict your path here
-	        return source;
-	    }
+	@Bean
+	public FilterRegistrationBean corsFilter() { 
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	    bean.setOrder(0);
+	    return bean;
+	}
 }
